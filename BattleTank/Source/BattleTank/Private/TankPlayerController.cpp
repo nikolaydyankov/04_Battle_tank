e7493766658b9ddef47813bd7996af0d37fe104d
Nikolay Dyankov <dyankov.nikolay@gmail.com>
2018-08-29 19:11:37 +0300

    log possessed tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -6,3 +6,17 @@
 ATank* ATankPlayerController::GetControllerTank() const {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControllerTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayController not possessing any tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayController possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
